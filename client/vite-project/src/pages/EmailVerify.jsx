869e997897285d0753637bab1fc12e793023edbc
Suryansh <shubamsharma44122@gmail.com>
2026-01-09 21:48:54 +0530

    made frontend for otp-verification and connect it to backend

--- client/vite-project/src/pages/EmailVerify.jsx
+++ client/vite-project/src/pages/EmailVerify.jsx
@@ -1,12 +1,79 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {assets} from '../assets/assets.js'
+import axios from 'axios'
+import { AppContent } from '../context/AppContext.jsx'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { useEffect } from 'react'
 const EmailVerify = () => {
+  axios.defaults.withCredentials=true
+  const{backendUrl,isLoggedIn,userData,getUserData}=useContext(AppContent)
+  const navigate=useNavigate()
+  const inputRefs=React.useRef([])//when we add otp in boxes it will get store here
+  const handleInput=(e,index)=>{//when we enter otp one bo one into boxes we have to click them manuallt to go to next.After using this fn whenever we wnter number in a box it will automatically shift to next one
+    if(e.target.value.length>0 && index<inputRefs.current.length-1){
+      inputRefs.current[index+1].focus();
+    }
+    
+  }
+  const handleKeyDown=(e,index)=>{//when we press backspace the pointer will automatically move towards previous field
+    if(e.key==='Backspace' && e.target.value==='' && index>0){
+      inputRefs.current[index-1].focus();
+    }
+  }
+  //when we paste otp it will at once get paste into boxes
+  const handlePaste=(e)=>{
+    const paste=e.clipboardData.getData('text')
+    const pasteArray=paste.split('');
+    pasteArray.forEach((char,index)=>{
+      if(inputRefs.current[index]){
+        inputRefs.current[index].value=char
+      }
+    })
+  }
+  //handle submit button
+ const onSubmitHandler = async (e) => {
+  e.preventDefault();
+
+  try {
+    const otp = inputRefs.current.map(i => i.value).join('');
+    const res = await axios.post(
+      `${backendUrl}/api/auth/verifyEmail`,
+      { otp },
+      { withCredentials: true }
+    );
+
+    console.log(res.data);
+
+    toast.success(res.data.message);
+
+    await getUserData(); // wait for state update
+    navigate('/');
+
+  } catch (error) {
+    toast.error(error.response?.data?.message || error.message);
+  }
+};
+//if user has just enter otp and enter the home screen if he goes back the the emailVerify screen will be visible.In order to prevent that we use use effect
+useEffect(()=>{
+  isLoggedIn && userData && userData.isAccountVerified && navigate('/')
+},[isLoggedIn,userData])
   return (
-    <div className="min-h-screen px-6 sm:px-0 flex items-center justify-center bg-gradient-to-r from-blue-200 to-purple-400">
+    <div className="min-h-screen px-6 :px-0 flex items-center justify-center bg-gradient-to-r from-blue-200 to-purple-400">
     <img onClick={()=>Navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
-    <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
-    <h1>Email Verify OTP</h1>
-    <p>Enter the 6-digit code sent to your email id.</p>
+    <form onSubmit={onSubmitHandler} className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+    <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verify OTP</h1>
+    <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your email id.</p>
+    <div className='flex justify-between mb-8' onPaste={handlePaste}>
+      {Array(6).fill(0).map((_,index)=>(
+        <input type="text" maxLength='1' key={index} required 
+        className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md'
+        ref={e=>inputRefs.current[index]=e}
+        onInput={(e)=>handleInput(e,index)}
+        onKeyDown={(e)=>handleKeyDown(e,index)}/>
+      ))}
+    </div>
+    <button className='w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 rounded-full'>Verify email</button>
     </form>
     </div>
   )

c0a13a5fe8e4732f454b6adff4d35e1b3561123c
Suryansh <shubamsharma44122@gmail.com>
2026-01-10 22:55:00 +0530

    feat(auth): implement password reset with email OTP

--- client/vite-project/src/pages/EmailVerify.jsx
+++ client/vite-project/src/pages/EmailVerify.jsx
@@ -60,7 +60,7 @@ useEffect(()=>{
 },[isLoggedIn,userData])
   return (
     <div className="min-h-screen px-6 :px-0 flex items-center justify-center bg-gradient-to-r from-blue-200 to-purple-400">
-    <img onClick={()=>Navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+    <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
     <form onSubmit={onSubmitHandler} className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
     <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verify OTP</h1>
     <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your email id.</p>
